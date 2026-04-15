25ea321628310e00680d46a233a7e777298f49d8
sharavisshinde03 <sharavisshinde03@gmail.com>
2026-04-15 21:04:54 +0530

    fix render port issue

--- web/app.py
+++ web/app.py
@@ -245,5 +245,6 @@ def status():
 # -------------------------------------------------
 if __name__ == "__main__":
     threading.Thread(target=simulation_loop, daemon=True).start()
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
